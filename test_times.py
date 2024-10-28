12c0b57aaafdb0e59667bceee7e21951af37901d
Luke Amos <luke.amos.24@ucl.ac.uk>
2024-10-28 09:49:56 +0000

    Changing variable names to be more appropriate + adding start and end same time test.

--- test_times.py
+++ test_times.py
@@ -9,9 +9,9 @@ def test_given_input():
 
 def test_no_overlap():
     """Testing no overlap between inputs."""
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00")
-    result = compute_overlap_time(large, short)
+    time1 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    time2 = time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00")
+    result = compute_overlap_time(time1, time2)
     expected = []
     assert result==expected
 
@@ -21,3 +21,12 @@ def test_mult_intervals():
     result = compute_overlap_time(time, time)
     expected = [('2010-01-12 10:00:00', '2010-01-12 10:29:15'), ('2010-01-12 10:30:15', '2010-01-12 10:59:30'), ('2010-01-12 11:00:30', '2010-01-12 11:29:45'), ('2010-01-12 11:30:45', '2010-01-12 12:00:00')]
     assert result==expected
+
+def test_start_end_same():
+    """Testing one input ending when another starts."""
+    time1 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    time2 = time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00")
+    result = compute_overlap_time(time1, time2)
+    expected = [('2010-01-12 12:00:00', '2010-01-12 12:00:00')]
+    assert result==expected
+
