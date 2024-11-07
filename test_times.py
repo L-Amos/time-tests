d3a46f3b13fcf1a5196026e1a74c7d886c77885c
L-Amos <luke.amos77@outlook.com>
2024-11-07 18:13:48 +0000

    Adding and implementing yaml to store test fixtures.

--- test_times.py
+++ test_times.py
@@ -1,9 +1,15 @@
 from times import compute_overlap_time, time_range
 import pytest
+import yaml
 
-@pytest.mark.parametrize("input1, input2, expected", [(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00"), []), 
-                                                       (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 4, 60),time_range("2010-01-12 11:00:00", "2010-01-12 12:00:00", 2, 60), [('2010-01-12 11:00:30', '2010-01-12 11:29:30'), ('2010-01-12 11:30:45', '2010-01-12 12:00:00')]),
-                                                       (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00"), [('2010-01-12 12:00:00', '2010-01-12 12:00:00')])])
+
+with open("fixture.yaml") as f:
+    yaml_data = yaml.safe_load(f)
+
+paramaterized_data = [[time_range(*yaml_data[key]['time_range_1']), time_range(*yaml_data[key]['time_range_2']), [tuple(arr) for arr in yaml_data[key]['expected']]] for key in yaml_data.keys()]
+
+
+@pytest.mark.parametrize("input1, input2, expected", paramaterized_data)
 def test_given_input(input1, input2, expected):
     result = compute_overlap_time(input1, input2) 
     assert result == expected
